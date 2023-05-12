17aecb8da7e83246b2ea5aa0955cac397108431b
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-05-12 08:55:35 +0200

    fixed add category add_task

--- assets/templates/board_template.js
+++ assets/templates/board_template.js
@@ -129,7 +129,7 @@ function loadAddTaskTmp() {
                                         <p id="category-header">Select your Category</p>
                                         <img src="./assets/img/arrow_down.png">
                                     </div>
-                                    <div class="category-assigned-to d-none" id="category-choices" onclick="dontClose(event)">
+                                    <div class="category-assigned-to d-none" id="category-choices">
                                         <div class="category" onclick="openAddNewCategory('category-choices')">New Category<img class="new-category-img" src="assets/img/add_task_mob.svg">
                                     </div>
                                     <div class="category" onclick="changeCategoryHeader('Marketing')">
@@ -334,7 +334,7 @@ function loadEditAddTaskTmp(id) {
                 <div class="sector_top">
                     <p id="category-header">Select your Category</p><img src="./assets/img/arrow_down.png">
                 </div>
-                <div class="category-choices category-assigned-to d-none" id="category-choices" onclick="dontClose(event)">
+                <div class="category-choices category-assigned-to d-none" id="category-choices">
                     <div class="category" onclick="openAddNewCategory('category-choices')">New Category<img class="new-category-img" src="assets/img/add_task_mob.svg">
                     </div>
                     <div class="category" onclick="changeCategoryHeader('Marketing')">
